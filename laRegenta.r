9bfcc501bd65c946a7608de5171e582c243f232f
alvaro <elvarox301@gmail.com>
2022-01-29 18:17:26 +0100

    Read txt by lines and remove post-ending and pre-prologue

--- laRegenta.r
+++ laRegenta.r
@@ -15,3 +15,14 @@ library(quanteda)
 library(topicmodels)
 library(cvTools)
 
+
+# laRegenta <- read.delim("laRegenta.txt", encoding = "UTF-8")
+
+lines <- readLines("laRegenta.txt", encoding = "UTF-8")
+grep(pattern = "***", lines, fixed = TRUE)
+head(lines, 42) # Before prologue
+tail(lines, 366) # Ending
+lRegenta <- lines[42:34292]
+length(lRegenta) # text without pre-prologue and post-ending
+head(lRegenta)
+tail(lRegenta)
